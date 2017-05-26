cc25e719292626b923bbae4fe156ed7b58367fb8
Seon Hong Kim <scarletbreez@naver.com>
2017-05-27 00:10:04 +0900

    authuser

---
+++ webdiary만들기/project/js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyDVfzGj9effOPWHEawF4XoRUVR7EprnqeU",
+  authDomain: "webprogramming-27026.firebaseapp.com",
+  databaseURL: "https://webprogramming-27026.firebaseio.com",
+  projectId: "webprogramming-27026",
+  storageBucket: "webprogramming-27026.appspot.com",
+  messagingSenderId: "97991502624"
+  };
+firebase.initializeApp(config);
