acbfd0cf52e0a4627f604118aa3e8585f725702f
Seon Hong Kim <scarletbreez@naver.com>
2017-06-10 22:18:10 +0900

    final chaged

--- webdiary만들기/project/js/init.js
+++
@@ -1,9 +0,0 @@
-var config = {
-  apiKey: "AIzaSyDVfzGj9effOPWHEawF4XoRUVR7EprnqeU",
-  authDomain: "webprogramming-27026.firebaseapp.com",
-  databaseURL: "https://webprogramming-27026.firebaseio.com",
-  projectId: "webprogramming-27026",
-  storageBucket: "webprogramming-27026.appspot.com",
-  messagingSenderId: "97991502624"
-  };
-firebase.initializeApp(config);
